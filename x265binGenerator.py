7c9f12add38d465bbfae930c028dbf3997914f82
Dieison Silveira <dieison.s.silveira@gmail.com>
2017-01-18 15:52:46 -0200

    func encoder done :)

--- x265binGenerator.py
+++ x265binGenerator.py
@@ -2,7 +2,6 @@
 #encoding: UTF-8
 # Dieison Silveira
 
-#aulapinroot@beagle1:~/cacti65/cfg/old/.results/
 
 from subprocess import check_output, PIPE, STDOUT
 import os
@@ -13,13 +12,13 @@ frames=[]
 width=[]
 height=[]
 presets = ["ultrafast", "superfast", "veryfast", "faster", "fast", "medium", "slow", "slower", "veryslow", "placebo"]
-
+qp = "22"
 videoPath="~/Pesquisa/videos/1920x1080/"
-pcm="/home/aulapinroot/Programs/IntelPerformanceCounterMonitor-V2.11/pcm.x"
-hwCounters="L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,LLC-stores,LLC-store-misses,LLC-loads,LLC-loads-misses"
+# pcm="/Programs/IntelPerformanceCounterMonitor-V2.11/pcm.x"
+# hwCounters="L1-dcache-loads,L1-dcache-load-misses,L1-dcache-stores,L1-dcache-store-misses,LLC-stores,LLC-store-misses,LLC-loads,LLC-loads-misses"
 
 def load_in():
-	f = open('videos2.in','r')
+	f = open('videos.in','r')
 	line = f.readline()
 
 	while line:
@@ -32,24 +31,20 @@ def load_in():
 		line = f.readline()
 
 def encoder():
-	arq = open("simulation_encoder2.txt", "w")
+	arq = open("simulation_encoder1.txt", "w")
 	i=0	
 	for video in videos:
 		for preset in presets:
-			app = "encoder --input "+videoPath+video+".yuv --fps 50 --input-res "+width[i]+"x"+height[i]+" --frames "+frames[i]+" --preset " + preset + " --ssim --psnr --output "+videoPath+"bin/"+preset+video+".bin"
-			pcmcmd="sudo -E "+pcm+" --noJKTWA -r --external-program ./"+app+" >> pcmResults/"+preset+video+".log"
+			cmd = "./bin/x265 --input "+videoPath+"cct/"+video+".yuv --fps 50 --input-res "+width[i]+"x"+height[i]+" --frames "+frames[i]+" --qp "+qp+" --preset " + preset + " --ssim --psnr --output "+videoPath+"bin/"+qp+"_"+preset+"_"+video+".bin"
 			
-			print pcmcmd
-			outputpcm = check_output(pcmcmd, shell=True, stderr=STDOUT)
+			print cmd
+			outputcmd = check_output(cmd, shell=True, stderr=STDOUT)
 			
-			perfcmd="sudo -E perf stat --output perfResults/"+preset+video+".log -e "+hwCounters+" ./"+app     
-			print perfcmd
-			outputperf = check_output(perfcmd, shell=True, stderr=STDOUT)
-			
-			# arq.write("######### "+preset+" #########")
-			# arq.write("\n")
-			# arq.write(output)
-			# arq.write("\n\n")
+			arq.write("######### "+preset+" #########")
+			arq.write("\n")
+			arq.write(outputcmd)
+			arq.write("\n\n")
+		i+=1
 	arq.close()
 
 def decoder():
@@ -67,6 +62,7 @@ def decoder():
 
 if __name__ == "__main__":
 	# main(argv[1], int(argv[2]))
+	qp = argv[1]
 	load_in()
 	encoder()
 	# decoder()
